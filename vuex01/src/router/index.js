fb634ef48fc21ee91737eca70bc21b0528f87dc7
SZR-alt <2062279899@qq.com>
2020-10-10 15:45:58 +0800

    vuex01

--- vuex01/src/router/index.js
+++ vuex01/src/router/index.js
@@ -1,29 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Access1 from '../views/Access1.vue'
+import Access2 from '../views/Access2.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/access1',
+        component: Access1
+    },
+    {
+        path: '/access2',
+        component: Access2
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ vuex01/src/views/Access1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <h1>访问Vuex中的数据 -- 01页面</h1>
+     <p>姓名:{{this.$store.state.username}}</p>
+     <p>性别:{{this.$store.state.sex ? '男' : '女'}}</p>
+     <p>年龄:{{this.$store.state.age}}</p>
+     <p>工资:{{this.$store.state.salary}}</p>
+  </div>
+</template>
+<script>
+export default {
+  mounted(){
+    console.log(this.$store.state.username);
+    console.log(this.$store.state.age);
+    console.log(this.$store.state.sex);
+    console.log(this.$store.state.salary);
+  }
+}
+</script>
